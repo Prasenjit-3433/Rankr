eb8ddcd00a8c5bee23e6d96c8ec950e6a6414a44
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-08-29 11:27:12 +0530

    create-poll page in action

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { devtools } from 'valtio/utils';
+import { useSnapshot } from 'valtio';
 
 import './index.css';
 import Pages from './Pages';
 import { state } from './state';
+import Loader from './components/ui/Loader';
 
 devtools(state, 'App State');
-const App: React.FC = () => <Pages />;
+const App: React.FC = () => {
+  const currentState = useSnapshot(state);
+
+  return (
+    <>
+      <Loader isLoading={currentState.isLoading} color="orange" width={120} />
+      <Pages/>
+    </>
+  );
+};
 
 export default App;
--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -1,9 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CountSelector from '../components/ui/CountSelector';
+import { AppPage, actions } from '../state';
+import { makeRequest } from '../api';
+import { Poll } from 'shared/poll-types';
 
-const Create: React.FC = () => (
-  <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
-    <h3 className="text-center">Enter Poll Topic</h3>
-  </div>
-);
+const Create: React.FC = () => {
+  const [pollTopic, setPollTopic] = useState('');
+  const [maxVotes, setMaxVotes] = useState(3);
+  const [name, setName] = useState('');
+  const [apiError, setApiError] = useState('');
+
+  const areFieldsValid = (): boolean => {
+    if (pollTopic.length < 1 || pollTopic.length > 100) {
+      return false;
+    }
+
+    if (maxVotes < 1 || maxVotes > 5) {
+      return false;
+    }
+
+    if (name.length < 1 || name.length > 25) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleCreatePoll = async () => {
+    actions.startLoading();
+    setApiError('');
+
+    const { data, error } = await makeRequest<{
+      poll: Poll;
+      accessToken: string;
+    }>('/polls', {
+      method: 'POST',
+      body: JSON.stringify({
+        topic: pollTopic,
+        votesPerVoter: maxVotes,
+        name,
+      }),
+    });
+
+    console.log(data, error);
+
+    if (error && error.statusCode === 400) {
+      console.log('400 error', error);
+      setApiError('Name and Poll Topic are both required');
+    } else if (error && error.statusCode !== 400) {
+      setApiError(error.messages[0]);
+    } else {
+      actions.initializePoll(data.poll);
+      actions.setPollAccessToken(data.accessToken);
+      actions.setPage(AppPage.WaitingRoom);
+    }
+
+    actions.stopLoading();
+  };
+
+  return (
+    <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
+      <div className="mb-12">
+        <h3 className="text-center">Enter Poll Topic</h3>
+        <div className="text-center w-full">
+          <input
+            maxLength={100}
+            onChange={(e) => setPollTopic(e.target.value)}
+            className="box info w-full"
+          />
+        </div>
+
+        <h3 className="text-center mt-4 mb-2">Votes Per Participant</h3>
+        <div className="w-48 mx-auto my-4">
+          <CountSelector
+            initial={3}
+            min={0}
+            max={5}
+            step={1}
+            onChange={(val) => setMaxVotes(val)}
+          />
+        </div>
+
+        <div className="mb-12">
+          <h3 className="text-center">Enter Name</h3>
+          <div className="text-center w-full">
+            <input
+              maxLength={25}
+              onChange={(e) => setName(e.target.value)}
+              className="box info w-full"
+            />
+          </div>
+        </div>
+
+        {apiError && (
+          <p className="text-center text-red-600 font-light mt-8">{apiError}</p>
+        )}
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <button
+          className="box btn-orange w-32 my-2"
+          onClick={handleCreatePoll}
+          disabled={!areFieldsValid()}
+        >
+          Create
+        </button>
+
+        <button
+          className="box btn-purple w-32 my-2"
+          onClick={() => actions.startOver()}
+        >
+          Start Over
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Create;
