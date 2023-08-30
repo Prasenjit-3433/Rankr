96a4934f968eb2521ae93ad9afb9534a7ad23752
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-08-30 12:29:40 +0530

    connecting react client to NestJS backend via socket.io

--- client/src/state.ts
+++ client/src/state.ts
@@ -1,7 +1,9 @@
 import { Poll } from 'shared/poll-types';
-import { proxy } from 'valtio';
+import { proxy, ref } from 'valtio';
 import { derive, subscribeKey } from 'valtio/utils';
 import { getTokenPayload } from './util';
+import { Socket } from 'socket.io-client';
+import { createSocketWithHandlers, socketIOUrl } from './socket-io';
 
 export enum AppPage {
   Welcome = 'welcome',
@@ -21,6 +23,7 @@ export type AppState = {
   currentPage: AppPage;
   poll?: Poll;
   accessToken?: string;
+  socket?: Socket;
 };
 
 const state: AppState = proxy({
@@ -81,6 +84,22 @@ const actions = {
   setPollAccessToken: (token?: string): void => {
     state.accessToken = token;
   },
+  initializeSocket: (): void => {
+    if (!state.socket) {
+      state.socket = ref(
+        createSocketWithHandlers({
+          socketIOUrl,
+          state,
+          actions,
+        })
+      );
+    } else {
+      state.socket.connect();
+    }
+  },
+  updatePoll: (poll: Poll): void => {
+    state.poll = poll;
+  },
 };
 
 subscribeKey(state, 'accessToken', () => {
@@ -90,5 +109,7 @@ subscribeKey(state, 'accessToken', () => {
     localStorage.removeItem('accessToken');
   }
 });
+
+export type AppActions = typeof actions;
 
 export { stateWithComputed as state, actions };
